1a967c5163aac40d0cc9dd2cf3a9a6f89c2ecf7f
Elliot Nunn <elliotnunn@me.com>
2018-07-03 13:55:22 +0800

    test fixture

--- NanoKernel/NanoKernel.s
+++ NanoKernel/NanoKernel.s
@@ -10,61 +10,22 @@
 	include		'NKMacros.s'
 
 NKTop
-	include		'NKInit.s'
+	b			VMPanic
 
-	align		5
-IntPanicIsland
-	b			Panic
-	include		'NKExceptions.s'
-	align		5
-	include		'NKIntHandlers.s'
-	align		5
-	include		'NKFloatInts.s'
-	align		6
-	include		'NKIntMisc.s'
 
-	align		5
-	include		'NKPaging.s'
-	align		5
-	include		'NKTranslation.s'
-	align		5
+	
+	org			0x23AC
+IntReturn
+
+	org			0x18F8
+PagingFunc1
+
+	org			0x468
+VMPanic
+Panic
+
+	org			0x30cc
+
 	include		'NKVMCalls.s'
-	align		5
-	include		'NKPowerCalls.s'
-	align		5
-	include		'NKRTASCalls.s'
-	align		5
-	include		'NKCache.s'
 
-	;	Mostly MP calls:
-	align		5
-	include		'NKMPCalls.s'
-	align		5
-	include		'NKSync.s'
-	align		5
-	include		'NKTasks.s'
-	align		5
-	include		'NKAddressSpaces.s'
-
-	align		5
-	include		'NKPoolAllocator.s'
-	align		5
-	include		'NKTimers.s'
-	align		5
-	include		'NKScheduler.s'
-	align		5
-	include		'NKIndex.s'
-	align		5
-	include		'NKPrimaryIntHandlers.s'
-	align		5
-	include		'NKConsoleLog.s'
-	align		5
-	include		'NKSleep.s'
-	align		5
-	include		'NKThud.s'
-	align		5
-	include		'NKScreenConsole.s'
-	align		5
-	include		'NKAdditions.s'
-	align		5
 NKBtm
